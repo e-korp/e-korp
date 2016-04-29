7b990d45ec3801afffec116c269c182c37fd1d34
Johan Kanefur <johan.canefur@gmail.com>
2016-04-30 00:20:45 +0200

    Added logging configuration into separate module

---
+++ src/logging.js
@@ -0,0 +1,34 @@
+/**
+ * Setup logging
+ * We use winston for logging:
+ * https://github.com/winstonjs/winston#working-with-multiple-loggers-in-winston
+ */
+const winston = require('winston');
+
+// Configure logger for background tasks
+winston.loggers.add('tasklog', {
+  console: {
+    level: 'silly',
+    colorize: true,
+    label: 'Background tasks',
+  },
+  file: {
+    filename: './logs/bgtasks.log',
+  },
+});
+
+// Configure the logger for general use
+winston.loggers.add('applog', {
+  console: {
+    level: 'info',
+    colorize: true,
+    label: 'Application log',
+  },
+  file: {
+    filename: './logs/app.log',
+  },
+});
+
+// Report in that the logging is working
+const applog = winston.loggers.get('applog');
+applog.info('Logging setup successfully');

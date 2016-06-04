c3709308515d6f44fbd4af5b78d05ef889a4ebf5
Johan Kanefur <johan.canefur@gmail.com>
2016-06-04 11:56:34 +0200

    Modified category schema, cleaned up category router

--- entities/category/category-router.js
+++ entities/category/category-router.js
@@ -2,50 +2,37 @@ const router = require('express').Router();
 const async = require('asyncawait/async');
 const await = require('asyncawait/await');
 
-const CategoryCollection = require('./category-collection');
-const Category = require('./category-resource');
+const Category = require('./category-model');
+
+// Error handling and logging
+const Oops = require('../../lib/oops');
+const applog = require('winston').loggers.get('applog');
+
+// Middleware
+const authMiddleware = require('../../server/middleware/authentication');
 
 
 /**
- * Get categories
- */
-const getAll = async((req, res) => {
-  const cc = new CategoryCollection();
-
-  try {
-    const categories = await(cc.getAll());
-    res.status(200).json(categories);
-  } catch (e) {
-    res.status(400).json({
-      error: {
-        message: 'Could not fetch categories',
-      },
-    });
-  }
-});
-
-
-/**
- * Add new category
+ * Create a new category
  * @author Johan Kanefur <johan.canefur@gmail.com>
- * @todo Add middleware for userlevel
  */
-const create = async((req, res) => {
-  try {
-    const c = new Category(this.body.name);
-    await(c.save());
-
-    res.status(201).json(c);
-  } catch (e) {
-    res.status(400).json({
-      error: {
-        message: e.message,
-      },
-    });
-  }
+const create = async((req, res, next) => {
+  
 });
 
-// router.get('/', getAll);
-// router.post('/', create);
+
+
+/**
+ * Gets all categories that exists
+ * @author Johan Kanefur <johan.canefur@gmail.com>
+ */
+const getAll = async((req, res, next) => {
+
+
+});
+
+
+router.post('/', authMiddleware.admin, create);
+router.get('/', getAll);
 
 module.exports = router;

680ec0f54d50d1d368a4cea3a410303c3156a967
Johan Kanefur <johan.canefur@gmail.com>
2016-06-05 19:40:48 +0200

    Removed old product stuff

--- entities/product/product-router.js
+++ entities/product/product-router.js
@@ -3,40 +3,11 @@ const router = require('express').Router();
 const async = require('asyncawait/async');
 const await = require('asyncawait/await');
 
-// Import the resource
-const Product = require('./product-resource');
-const ProductCollection = require('./product-collection');
-
-/**
- * Get all products
- */
-const getAll = async((req, res) => {
-  const collection = new ProductCollection();
-
-  try {
-    await(collection.getAll());
-    res.status(200).json(collection.getData());
-  } catch (err) {
-    res.status(500).error('Could not get products', err);
-  }
-});
-
-
-/**
- * Get specific product
- */
-const getSpecific = async((req, res) => {
-  try {
-    const product = await(Product.getById(this.params.id));
-    res.status(200).json(product);
-  } catch (err) {
-    res.status(404).error('Could not find product', err);
-  }
-});
+// Import the models
+const Product = require('./product-model');
 
 // Mount routers
 // router.get('/', getAll);
-// router.get('/:id', getSpecific);
 
 module.exports = {
   router: router

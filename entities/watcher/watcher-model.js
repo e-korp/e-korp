759a8e6c1d9bf07409633079ed6290ad70b0bf75
Johan Kanefur <johan.canefur@gmail.com>
2016-06-07 09:54:24 +0200

    Documented error codes, fixed error handling for watchers

--- entities/watcher/watcher-model.js
+++ entities/watcher/watcher-model.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const states = require('./states');
-const applog = require('winston').loggers.get('applog');
 
 const schema = new mongoose.Schema({
   name: {
@@ -25,15 +24,6 @@ const schema = new mongoose.Schema({
   }],
 }, {
   timestamps: true,
-});
-
-schema.pre('find', function(next) {
-  // TODO: Check for cached entry based on query params
-  next();
-});
-
-schema.post('find', function(next) {
-  // TODO: Add the record to the cache (on create and update aswell)
 });
 
 module.exports = mongoose.model('Watcher', schema);
